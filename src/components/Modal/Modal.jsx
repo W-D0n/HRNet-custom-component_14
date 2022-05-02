13830606a7cbf054fcb232d20352cf8c15c5a286
Kamal <kha0s@free.fr>
2022-05-02 16:24:37 +0400

    feat : add close logic inside component

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import "./Modal.css"
 
-export const Modal = ({ component, closeAction, clickOutsideAction }) => {
+export const Modal = ({ component, setModal }) => {
+  
+  const handleClick = () => {
+    setModal(false);
+  }
   return (
-    <div onClick={clickOutsideAction}>
+    <div onClick={handleClick}>
       <div className="modal-overlay" />
       <div className="modal-wrapper" aria-modal aria-hidden tabIndex={-1} role="dialog">
         <div className="modal">
           {component}
-          <button type="button" className="modal-close-button" data-dismiss="modal" aria-label="Close" onClick={closeAction}>
+          <button type="button"
+            className="modal-close-button"
+            data-dismiss="modal"
+            aria-label="Close"
+            onClick={handleClick}
+          >
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
